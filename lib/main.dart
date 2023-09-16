bbd1b8cbe6e0326ff42d6069ddfe10a4d36d87e1
Joexwilly <joeguyski@gmail.com>
2023-09-16 12:36:37 +0100

    added navigation from category

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,11 +21,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: theme,
-        home: MealsScreen(
-          meals: dummyMeals,
-          title: 'come ',
-        ));
+    return MaterialApp(theme: theme, home: const CategoriesScreen());
   }
 }
