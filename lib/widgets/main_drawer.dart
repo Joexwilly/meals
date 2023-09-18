baa606b83284595cce91709a6fd6fa1c694da483
Joexwilly <joeguyski@gmail.com>
2023-09-18 20:01:27 +0100

    minor adjustment

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -32,7 +32,9 @@ class MainDrawer extends StatelessWidget {
                   size: 49,
                   color: Theme.of(context).colorScheme.primary,
                 ),
-                const SizedBox(width: 18),
+                const SizedBox(
+                  width: 19,
+                ),
                 Text(
                   'Cooking Up!',
                   style: Theme.of(context).textTheme.titleLarge!.copyWith(
