05ca06effaa88c4605c4339c83e05edf566b07b7
Joexwilly <joeguyski@gmail.com>
2023-09-18 19:52:54 +0100

    icon size modification

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -19,7 +19,7 @@ class MainDrawer extends StatelessWidget {
                   Theme.of(context)
                       .colorScheme
                       .primaryContainer
-                      .withOpacity(0.8)
+                      .withOpacity(0.9)
                 ],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
@@ -29,7 +29,7 @@ class MainDrawer extends StatelessWidget {
               children: [
                 Icon(
                   Icons.fastfood,
-                  size: 48,
+                  size: 49,
                   color: Theme.of(context).colorScheme.primary,
                 ),
                 const SizedBox(width: 18),
